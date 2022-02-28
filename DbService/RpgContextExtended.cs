eed3ce77b9635b0d4eef6babffc198b175ff41f8
PaakuPakuu <hereng.jordan@outlook.fr>
2022-02-28 21:52:09 +0100

    Data backups

--- DbService/RpgContextExtended.cs
+++ DbService/RpgContextExtended.cs
@@ -4,13 +4,16 @@ namespace DbService
 {
     public partial class RpgContext : DbContext
     {
-        private const string CONNECTION_STRING = "server=localhost;database=rpg_jh_local;port=3306;user id=root;password=root";
+        private const string LOCAL_CONNECTION_STRING = "server=localhost;database=rpg_jh_local;port=3306;user id=root;password=root";
+        private const string HEROKU_CONNECTION_STRING = "server=eu-cdbr-west-02.cleardb.net;database=heroku_b8db28dbd295a67;port=3306;user id=bd1a1c8389c3ab;password=fdd93476";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            string connectionString = HEROKU_CONNECTION_STRING;
+
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseMySql(CONNECTION_STRING, ServerVersion.AutoDetect(CONNECTION_STRING));
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
             }
         }
     }
